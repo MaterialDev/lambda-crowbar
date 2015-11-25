899cfa9f816d8e0396a2b71c2f0b6adcdd5833a7
Chris Patten <chris_patten@volusion.com>
2015-11-25 11:29:20 -0600

    Stupid...

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ exports.deploy = function(codePackage, config, callback, logger, lambda) {
           });
         }
     });
-  };
+  }};
 
   var updateEventSource = function(callback) {
     if(!config.eventSource) {
