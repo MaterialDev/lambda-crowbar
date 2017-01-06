c8a2eb63c316daf2c728a23254098fb8403ea0a8
jamesallen-vol <james.allen@volusion.com>
2017-01-06 14:38:22 -0600

    return a promise

--- src/index.js
+++ src/index.js
@@ -301,7 +301,7 @@ const createOrUpdateIAMRole = (iamClient, params) => {
       console.log(`roleResponse`);
       console.log(JSON.stringify(roleResponse, null, 2));
       role = roleResponse;
-      return policies.mapSeries(policies, policy => {
+      return Promise.mapSeries(policies, policy => {
         console.log(JSON.stringify(policy, null, 2));
         const localParams = {
           PolicyDocument: policy.PolicyDocument,
